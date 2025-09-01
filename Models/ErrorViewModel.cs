dcc7fcd97514d360fe6b63d1617f5fc05d65462b
OfficialProtonDev <billycodez@gmail.com>
2025-09-01 13:48:30 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProntoPizzas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
