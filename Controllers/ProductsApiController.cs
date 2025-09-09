cd8a6212696eb7505f30e1cac8ceb22e407d5390
OfficialProtonDev <billycodez@gmail.com>
2025-09-10 10:01:13 +1200

    Added API endpoints & Swagger

---
+++ Controllers/ProductsApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProntoPizzas.Data;
+using ProntoPizzas.Models;
+
+namespace ProntoPizzas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        // GET: api/ProductsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(Guid id)
+        {
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+    }
+}
