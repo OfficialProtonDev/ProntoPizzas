628fdd35f5bb4dfab2185114719fe46a2d4ed4ac
OfficialProtonDev <billycodez@gmail.com>
2025-09-10 11:21:49 +1200

    Added role access limitations to all pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProntoPizzas.Models;
+using System.Diagnostics;
 
 namespace ProntoPizzas.Controllers
 {
+    [Authorize(Roles = "Staff, Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProntoPizzas.Data;
 using ProntoPizzas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProntoPizzas.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
