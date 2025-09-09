a71819cae30073b28a769380953175e0781c49a3
OfficialProtonDev <billycodez@gmail.com>
2025-09-09 14:48:07 +1200

    Added OrderManagement page for easy status updates for staff.

---
+++ Models/OrderManagementIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProntoPizzas.Models
+{
+    public class OrderManagementIndexViewModel
+    {
+        public List<Order> Orders { get; set; }
+    }
+
+}
